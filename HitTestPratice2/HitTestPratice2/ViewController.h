d965ca4e1d4d9cc29a5db0197c0807d39e80507a
susu <28025920@qq.com>
2016-04-20 23:25:50 +0800

    创建项目，添加素材

---
+++ HitTestPratice2/HitTestPratice2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HitTestPratice2
+//
+//  Created by  江苏 on 16/4/20.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HitTestPratice2/HitTestPratice2/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  HitTestPratice2
+//
+//  Created by  江苏 on 16/4/20.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIButton *playButton;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (IBAction)play:(UIButton *)sender {
+}
+
+@end
